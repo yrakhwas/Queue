fcb09d88b65c2d55ce86e2dc5ffc25ad1e993f4f
yrakhwas <yrakhwas@gmail.com>
2024-11-01 15:47:02 +0200

    Add project files.

---
+++ Queue.cpp
@@ -0,0 +1,72 @@
+﻿#include <iostream>
+using namespace std;
+
+template <class T>
+class Queue {
+    T* data;
+    int maxSize;
+    int size;
+public:
+    Queue() = default;
+	Queue(int maxSize) : maxSize(maxSize), size(0) {
+		data = new T[maxSize];
+	}
+	~Queue()
+	{
+		if(data!=nullptr)
+			delete[] data;
+	}
+	void Enqueue(T elem) 
+	{
+		if (!IsFull())
+		{
+			data[size++] = elem;
+		}
+		else {
+			cout << "Queue is full" << endl;
+		}
+	}
+	bool IsFull() const
+	{
+		return size == maxSize;
+	}
+	bool IsEmpty()const
+	{
+		return size == 0;
+	}
+	T Dequeue()
+	{
+		if (!IsEmpty())
+		{
+			T firstElem = data[0];
+			for (int i = 0; i < size; i++)
+			{
+				data[i - 1] = data[i];
+			}
+			--size;
+			//data[size - 1] = firstElem;
+			return firstElem;
+		}
+	}
+	int GetCount()const
+	{
+		return size;
+	}
+	void Clear()
+	{
+		size = 0;
+	}
+	void Show()const
+	{
+		for (int i = 0; i < size; i++)
+		{
+			cout << data[i] << " ";
+		}
+		cout << endl;
+	}
+};
+
+int main()
+{
+	//Створити чергу на 25 елементів типу int, заповнити її рандомними числами від 1 до 100, продемонструвати роботу методів черги.
+}
